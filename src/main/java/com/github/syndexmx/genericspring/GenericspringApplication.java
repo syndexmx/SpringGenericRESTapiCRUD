b83706f69a69dc960396042f5e4d94b3ae906f86
Maxim Shapovalov <syndex@list.ru>
2024-10-14 15:06:35 +0300

    Initial commit

---
+++ src/main/java/com/github/syndexmx/genericspring/GenericspringApplication.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.genericspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericspringApplication.class, args);
+	}
+
+}
