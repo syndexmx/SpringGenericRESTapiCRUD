eb2e9ba8548300d1888248372fccc8ada99ce124
Maxim Shapovalov <syndex@list.ru>
2024-10-16 18:03:43 +0300

    Creation Controller test Fails

--- src/main/java/com/github/syndexmx/genericspring/controllers/GenericController.java
+++ src/main/java/com/github/syndexmx/genericspring/controllers/GenericController.java
@@ -1,8 +1,16 @@
 package com.github.syndexmx.genericspring.controllers;
 
+import com.github.syndexmx.genericspring.domain.Generic;
+import com.github.syndexmx.genericspring.dtos.GenericDto;
 import com.github.syndexmx.genericspring.services.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.github.syndexmx.genericspring.dtos.GenericDto.genericToGenericDto;
 
 @RestController
 public class GenericController {
@@ -12,6 +20,13 @@ public class GenericController {
     @Autowired
     private GenericController(GenericService genericService) {
         this.genericService = genericService;
+    }
+
+    @PostMapping("/api/v0/generics")
+    public ResponseEntity<GenericDto> createEntity(@RequestBody final Generic generic) {
+        final ResponseEntity<GenericDto> responseEntity = new ResponseEntity<> (
+                genericToGenericDto(genericService.create(generic)), HttpStatus.CREATED);
+        return responseEntity;
     }
 
 }
---
+++ src/test/java/com/github/syndexmx/genericspring/controllers/GenericControllerIT.java
@@ -0,0 +1,43 @@
+package com.github.syndexmx.genericspring.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.syndexmx.genericspring.domain.Generic;
+import com.github.syndexmx.genericspring.domain.TestGenericSupplier;
+import com.github.syndexmx.genericspring.dtos.GenericDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+public class GenericControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testThatGenericIsCreated() throws Exception {
+        final Generic generic = TestGenericSupplier.getTestGeneric();
+        final ObjectMapper objectMappper = new ObjectMapper();
+        final GenericDto genericDto = GenericDto.genericToGenericDto(generic);
+        final String genericJson = objectMappper.writeValueAsString(genericDto);
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/v0/generics")
+                .contentType(MediaType.APPLICATION_JSON)
+                        .content(genericJson))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().json(genericJson));
+    }
+
+}
