621d521e60d674ab664d0d571e735eb55439d4a9
Maxim Shapovalov <syndex@list.ru>
2024-10-16 19:45:17 +0300

    Retrieve (FindById) Controller implemented

--- src/test/java/com/github/syndexmx/genericspring/controllers/GenericControllerIT.java
+++ src/test/java/com/github/syndexmx/genericspring/controllers/GenericControllerIT.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.syndexmx.genericspring.domain.Generic;
 import com.github.syndexmx.genericspring.domain.TestGenericSupplier;
 import com.github.syndexmx.genericspring.dtos.GenericDto;
+import com.github.syndexmx.genericspring.services.GenericService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.UUID;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 @SpringBootTest
@@ -28,6 +31,9 @@ public class GenericControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private GenericService genericService;
 
     @Test
     public void testThatGenericIsCreated() throws Exception {
@@ -42,4 +48,24 @@ public class GenericControllerIT {
                 .andExpect(MockMvcResultMatchers.content().json(genericJson));
     }
 
+    @Test
+    public void testThatRetrieveReturnsNotFoundWhenAbsent() throws Exception {
+        final Generic generic = TestGenericSupplier.getTestNonExistentGeneric();
+        final UUID genericId = generic.getGenericId();
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v0/generics/" + genericId))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
+    @Test
+    public void testThatRetrieveReturnsGenericWhenExists() throws Exception {
+        final Generic generic = TestGenericSupplier.getTestGeneric();
+        final Generic genericSaved =genericService.create(generic);
+        final UUID genericId = genericSaved.getGenericId();
+        final GenericDto genericDto = GenericDto.genericToGenericDto(genericSaved);
+        final ObjectMapper objectMappper = new ObjectMapper();
+        final String genericJson = objectMappper.writeValueAsString(genericDto);
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v0/generics/" + genericId))
+                .andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(MockMvcResultMatchers.content().json(genericJson));
+    }
 }
