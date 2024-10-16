7da7ceeeb05ea2015a17f1219af6cf44667b369c
Maxim Shapovalov <syndex@list.ru>
2024-10-16 16:10:26 +0300

    Entity and Dto builders

--- src/main/java/com/github/syndexmx/genericspring/entities/GenericEntity.java
+++ src/main/java/com/github/syndexmx/genericspring/entities/GenericEntity.java
@@ -1,10 +1,12 @@
 package com.github.syndexmx.genericspring.entities;
 
+import com.github.syndexmx.genericspring.domain.GenericPojo;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,15 +15,33 @@ import java.util.UUID;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Data
 @Table(name = "generics_table")
 public class GenericEntity {
 
     @Id
     @Column(name = "generic_id")
-    private UUID genericPojoId;
+    private UUID genericId;
 
     @Column(name = "generic_string")
     private String genericString;
+
+    public GenericEntity genericPojoToGenericEntity(GenericPojo genericPojo) {
+        final GenericEntity genericEntity = GenericEntity.builder()
+                .genericId(genericPojo.getGenericId())
+                .genericString(genericPojo.getGenericString())
+                .build();
+        return genericEntity;
+    }
+
+    public GenericPojo genericPojo(GenericEntity genericEntity) {
+        GenericPojo genericPojo = GenericPojo.builder()
+                .genericId(genericEntity.getGenericId())
+                .genericString(genericEntity.getGenericString())
+                .build();
+        return genericPojo;
+
+    }
 
 }
