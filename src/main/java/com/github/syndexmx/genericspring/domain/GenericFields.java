fc819e005165ff30ffed7340c4ee8c41b3a8920e
Maxim Shapovalov <syndex@list.ru>
2025-03-28 17:09:13 +0300

    Content Fields changed

---
+++ src/main/java/com/github/syndexmx/genericspring/domain/GenericFields.java
@@ -0,0 +1,10 @@
+package com.github.syndexmx.genericspring.domain;
+
+import com.github.syndexmx.genericspring.annotations.TemplatedAnnotation;
+
+@TemplatedAnnotation
+public enum GenericFields {
+    DEFAULTVALUE,
+    ALTERNATIVEVALUE,
+    OTHERVALUE;
+}
