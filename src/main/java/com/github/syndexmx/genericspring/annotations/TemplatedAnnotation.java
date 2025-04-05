f8792e61e7ad9d9e29fe86816fff29c23491b119
Maxim Shapovalov <syndex@list.ru>
2025-04-05 23:19:16 +0300

    TemplatedAnnotation class refactored

--- src/main/java/com/github/syndexmx/genericspring/annotations/TemplatedAnnotation.java
+++ src/main/java/com/github/syndexmx/genericspring/annotations/TemplatedAnnotation.java
@@ -1,5 +1,7 @@
 package com.github.syndexmx.genericspring.annotations;
 
+import org.springframework.util.StringUtils;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,38 +14,46 @@ import static java.util.Map.entry;
 @Retention(RetentionPolicy.SOURCE)
 public @interface TemplatedAnnotation {
 
-    String genericPackageAddress = "com.github.syndexmx.genericspring";
-    String targetPackageAddress = "com.github.syndexmx.targetspring"; //change here
-
-    String genericObjectClass = "GenericObject";
-    String targetObjectClass = "Target"; //change here
-    String genericObjectLowCaseName = "genericObject";
-    String targetObjectLowCaseName = "target"; //change here
-
-    String genericType = "Generic";
-    String targetType = "Target"; //change here
-
-    String genericLowCaseName = "generic";
-    String targetLowCaseName = "target"; //change here
+    String TARGET_CLASS_NAME = "Target"; // Change here
 
     String genericIdType = "UUID";
     String targetIdType = "UUID"; //change here
+
+    String genericFieldType = "Short";
+    String genericFieldName = "genericField";
+    Map<String, String> fieldsMap = Map.ofEntries( // Object Fields block
+            entry("targetLong", "Long"), // Change here
+            entry("targetString", "String"), // Change here
+            entry("targetInteger", "Integer"), // Change here
+            entry("targetBoolean", "Boolean") // Change here
+    );
+
+    String genericPackageAddress = "com.github.syndexmx.genericspring";
+    String targetPackageAddress = "com.github.syndexmx.targetspring"; //change here
 
     String genericDependencyType = "GenericDependency";
     String targetDependencyType = "TargetDependency"; //change here
     String genericDependencyVariable = "genericDependency";
     String targetDependencyVariable = "targetDependency"; //change here
 
-    String genericFieldType = "Short";
-    String genericFieldName = "genericField";
-    Map<String, String> fieldsMap = Map.ofEntries( // Object Fields block
-            entry("targetStringVariable", "String"), // Change here
-            entry("targetIntegerVariable", "Integer"), // Change here
-            entry("targetBooleanVariable", "Boolean") // Change here
-    );
+
+    String targetClass = StringUtils.capitalize(TARGET_CLASS_NAME);
+    String targetClassCamel = StringUtils.uncapitalize(TARGET_CLASS_NAME);
+    String targetClassUpper = StringUtils.uncapitalize(TARGET_CLASS_NAME);
+
+    String genericObjectClass = "GenericObject";
+    String targetObjectClass = targetClass;
+    String genericObjectLowCaseName = "genericObject";
+    String targetObjectLowCaseName = targetClassCamel;
+
+    String genericType = "Generic";
+    String targetType = targetClass;
+
+    String genericLowCaseName = "generic";
+    String targetLowCaseName = targetClass;
 
     String genericAllUpperCaseName = "GENERIC";
-    String targetAllUpperCaseName = "TARGET"; //change
+    String targetAllUpperCaseName = targetClassUpper;
 
     /*
 
@@ -81,9 +91,5 @@ public @interface TemplatedAnnotation {
     String targetAllUpperCaseName = "TARGET"; //change
 
      */
-
-@TemplatedAnnotation
-    class InnerTemplatedAnnotationClass {
-    }
 
 }
